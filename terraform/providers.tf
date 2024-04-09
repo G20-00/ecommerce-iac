6e28b47225b31fc527e9b0f22faf874709770f6e
Gianni <sb5021087@gmail.com>
2024-04-09 18:42:59 -0500

    fix: deleted vars on container and key name

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,8 +2,8 @@ terraform {
   backend "azurerm" {
     resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
-    container_name       = var.state_storage_container_name
-    key                  = var.state_key
+    container_name       = "autoecommercesc"
+    key                  = "auto-terraform.tfstate"
   }
   required_providers {
     azurerm = {
