6975dbc21e3f4df38dfcd1b160c94d89a41f856a
Gianni <70019070+G20-00@users.noreply.github.com>
2024-05-05 21:07:03 -0500

    fix: sku and tags

--- terraform/modules/azurerm/mysql_flexible_server/main.tf
+++ terraform/modules/azurerm/mysql_flexible_server/main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_mysql_flexible_server" "example" {
+resource "azurerm_mysql_flexible_server" "mysql" {
   name                   = "example-mysqlserver"
   resource_group_name    = var.rg_name
   location               = var.rg_location
   version                = "5.7"
-  sku_name               = "Standard_D2s_v3"
   administrator_login    = "mysqladmin"
   administrator_password = "H@Sh1CoR3!"
+  tags                   = var.mysql_tags
 
   delegated_subnet_id = var.snet_id
 
