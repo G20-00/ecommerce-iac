446d2e602bbbe370f67d325f6a252738cec61c62
Gianni Benavides <sb5021087@gmail.com>
2024-05-15 19:10:59 -0500

    fix: change public network access

--- terraform/modules/azurerm/mysql_flexible_server/main.tf
+++ terraform/modules/azurerm/mysql_flexible_server/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   tags                   = var.mysql_tags
   sku_name               = "GP_Standard_D2ds_v4"
 
-  public_network_access_enabled = false
+  public_network_access_enabled = true
 
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
