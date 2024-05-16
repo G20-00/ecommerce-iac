a2af18022c81a503ec6ffb15098632c8b434d1db
Gianni <70019070+G20-00@users.noreply.github.com>
2024-05-15 19:56:38 -0500

    feat:add rule mysql firewall (#15)
    * feat:add rule mysql firewall
    
    * fix: name firewall
    
    * fix: rule name firewall

---
+++ terraform/modules/azurerm/mysql_firewall/variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "mysql_name" {
+  description = "value of the mysql server name"
+  type        = string
+}
--- terraform/modules/azurerm/mysql_flexible_server/output.tf
+++ terraform/modules/azurerm/mysql_flexible_server/output.tf
@@ -1,3 +1,7 @@
 output "mysql_id" {
   value = azurerm_mysql_flexible_server.mysql.id
 }
+
+output "mysql_name" {
+  value = azurerm_mysql_flexible_server.mysql.name
+}
