6a88a53ffca8471e9c451064cf5a2887c256b347
Gianni <sb5021087@gmail.com>
2024-05-29 18:38:05 -0500

    feat: update providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,20 +10,30 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~>2.11.0"
+    }
   }
 }
 
 data "azurerm_client_config" "current" {}
 
 data "azurerm_kubernetes_cluster" "cluster" {
-  depends_on = [
-    module.aks,
-    module.rg
-  ]
   name                = module.aks.aks_name
   resource_group_name = module.rg.rg_name
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "helm" {
+  kubernetes {
+    host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+  }
 }
